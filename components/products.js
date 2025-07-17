1b0bffb38abddcfc23dc480e2a9ba4b4dd5547ff
ayush99336 <ayushkaushikhere@gmail.com>
2025-07-17 10:09:46 +0530

    dummy layout

---
+++ components/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: '1',
+    name: 'Sunset',
+    price: 25,
+    image: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: '2',
+    name: 'Mountains',
+    price: 40,
+    image: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: '3',
+    name: 'Beach',
+    price: 30,
+    image: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: '4',
+    name: 'Cityscape',
+    price: 35,
+    image: 'https://picsum.photos/seed/picsum/200/300',
+  },
+];
+
+export default products;
